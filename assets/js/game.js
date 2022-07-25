e7bfaff94ae05488d62e5888b1d05ec640a5d03e
yaya <mamadou1991@outlook.com>
2022-07-25 18:56:19 -0500

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,24 @@
+//window.alert("This is an alert! JavaScript is running!");
+
+//function fight() {
+    window.alert("The fight has begun!");
+
+
+
+//window.prompt("What is your robot's name?");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fight();
